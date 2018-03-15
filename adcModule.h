364f2ae40d07d608813dd8d4bf5e73baad9db270
Samuel <sjh310@uclive.ac.nz>
2018-03-15 23:58:41 +1300

    Tidy code and add comments

--- adcModule.h
+++ adcModule.h
@@ -1,26 +1,43 @@
-#ifndef ADC_MODULE
-#define ADC_MODULE
+#ifndef ADC_MODULE_H_
+#define ADC_MODULE_H_
 
 // ***********************************************************************
-// adcModule.c
+// adcModule.h
 //
 // Helicopter project
 //
 // Group:       A03 Group 10
 // Created:     14/3/18
-// Last Edited: 14/3/18
+// Last Edited: 15/3/18
 //
 // Purpose: Initialise and handle analogue to digital conversion (ADC)
 // ************************************************************************
 
 #include <stdint.h>
 
+//Function pointer definition for the specifiable ADC value handler
 typedef void (*valueHandler_t)(uint32_t);
 
+
+//************************************************************************
+// Interrupt handler for completion of ADC conversion
+// Resets interrupt and registers ADC value with ADCValueHandler function
+//************************************************************************
 void ADCIntHandler(void);
 
+
+//************************************************************************
+// Initiate an ADC conversion
+//************************************************************************
 void triggerADC(void);
 
+
+//************************************************************************
+// Initialize ADC and register interrupt handler
+//
+// Parameters:
+// -valueHandler_t handler -> handler function called to store ADC value
+//************************************************************************
 void initADC(valueHandler_t);
 
 #endif
