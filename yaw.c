82ae64b5d8d5e429cc22f9425f06c665adedbd89
Samuel <sjh310@uclive.ac.nz>
2018-04-16 08:18:12 +1200

    Wrote untested yawGetDegrees function

--- yaw.c
+++ yaw.c
@@ -28,12 +28,13 @@
 #include "yaw.h"
 
 
+#define COUNTS_PER_ROTATION 112*4 //112 slots and x4 because quadrature encoding used
+#define DEGREES_PER_COUNT   360/COUNTS_PER_ROTATION
+
 int32_t yawGetDegrees(void)
 {
     int32_t rawCounts = quadEncoderGetCount();
-    int32_t angle = rawCounts; /*add appropriate calculation*/
-
-    return angle;
+    return rawCounts * DEGREES_PER_COUNT;
 }
 
 void yawCalibrate(void) //perhaps not part of yaw module???
