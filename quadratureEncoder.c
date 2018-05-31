9d0d5e4da468900063732a4f1966db85f362ef14
Samuel <sjh310@uclive.ac.nz>
2018-06-01 02:35:42 +1200

    Commented quadrature encoder and yaw modules

--- quadratureEncoder.c
+++ quadratureEncoder.c
@@ -1,163 +1,124 @@
- //************************************************************************
-// quadratureEncoder.c
-//
+//************************************************************************
+// quadratureEncoder.h
 // Helicopter project
-//
-// Group:       A03 Group 10
-// Created:     16/4/18
-// Last Edited: 10/5/18
+// Group: A03 Group 10
+// Last Edited: 31-5-18
 //
 // Purpose: Handles input from the quadrature encoder
 //************************************************************************
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h>
-
-// TivaWare library includes
-#include "inc/hw_memmap.h"        // defines GPIO_PORTF_BASE
-#include "inc/tm4c123gh6pm.h"     // defines interrupt vectors and register addresses
-#include "driverlib/gpio.h"       // defines for GPIO peripheral
-#include "driverlib/sysctl.h"     // system control functions
-#include "driverlib/systick.h"
-#include "driverlib/interrupt.h"
-
 #include "quadratureEncoder.h"
 
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+// Define constants for A and B encoder channel GPIO pins (PB0 and PB1 respectively)
 #define GPIO_CHANNEL_PERIPH     SYSCTL_PERIPH_GPIOB
 #define CHANNEL_PORT_BASE       GPIO_PORTB_BASE
 #define CHANNEL_A_PIN           GPIO_PIN_0
 #define CHANNEL_B_PIN           GPIO_PIN_1
 
-//#define GPIO_REF_PERIPH         SYSCTL_PERIPH_GPIOC
-//#define REF_PORT_BASE           GPIO_PORTC_BASE
-//#define REF_PIN                 GPIO_PIN_4
-
 #define CW_DIRECTION    1
 #define CCW_DIRECTION  -1
-#define INITIAL_INT_STATUS  0xFFFFFFFF
-
-
+#define INITIAL_INT_STATUS  0xFFFFFFFF // A normally invalid interrupt status value to
+                                       //  indicate if it is the first time the interrupt
+                                       //  has been triggered
 static uint32_t initialPinState;
 static volatile int32_t direction, encoderCount = 0;
 static volatile uint32_t lastIntStatus = INITIAL_INT_STATUS;
-//static volatile bool isCalibrated;
 
 
-
+// Handles interrupts from A and B channels of the quadrature encoder
+// Adds or subtracts from encoderCount depending on current rotation direction
 void quadEncoderIntHandler(void)
 {
-    uint32_t intStatus = GPIOIntStatus(CHANNEL_PORT_BASE, true);
+    uint32_t intStatus = GPIOIntStatus(CHANNEL_PORT_BASE, true); // Gets the pin that triggered
+                                                                 // the interrupt
+    // Clear the interrupt
     GPIOIntClear(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN);
 
-    if(lastIntStatus == INITIAL_INT_STATUS)
-    {
-        //as only one of intChannelA or intChannelB will be true and they will
-        //not both be true at the same time, only one of the two variables
-        //are needed. Both are included here for clarity & readability
+    if(lastIntStatus == INITIAL_INT_STATUS) // If this is the first interrupt, determine
+    {                                       //  the initial direction of rotation
+
+        // As only one of intChannelA or intChannelB can be true at the same time,
+        //  only one of the two variables are needed.
+        //  Both are included here for clarity & readability
         bool intChannelA = intStatus & CHANNEL_A_PIN;
         bool intChannelB = intStatus & CHANNEL_B_PIN;
 
         bool channelA = initialPinState & CHANNEL_A_PIN;
         bool channelB = initialPinState & CHANNEL_B_PIN;
 
-        if(intChannelA && channelA) //A rising edge
+        if(intChannelA && channelA) // A rising edge
         {
             !channelB ? (direction = CW_DIRECTION) : (direction = CCW_DIRECTION);
         }
-        else if(intChannelA && !channelA) //A falling edge
+        else if(intChannelA && !channelA) // A falling edge
         {
             channelB ? (direction = CW_DIRECTION) : (direction = CCW_DIRECTION);
         }
-        else if(intChannelB && channelB) //B rising edge
+        else if(intChannelB && channelB) // B rising edge
         {
             channelA ? (direction = CW_DIRECTION) : (direction = CCW_DIRECTION);
         }
-        else if(intChannelB && !channelB) //B falling edge
+        else if(intChannelB && !channelB) // B falling edge
         {
             !channelA ? (direction = CW_DIRECTION) : (direction = CCW_DIRECTION);
         }
     }
-    else if (lastIntStatus == intStatus)
-    {
+    else if (lastIntStatus == intStatus) // If the same pin triggers an interrupt twice
+    {                                    //  in succession then direction must have changed
+        // Reverse the direction
         direction *= -1;
     }
 
-    encoderCount += direction;
+    encoderCount += direction; // Either add or subtract one depending on direction
     lastIntStatus = intStatus;
 }
 
 
-
-/*void quadEncoderRefIntHandler(void)
-{
-    isCalibrated = true;
-    encoderCount = 0;
-    GPIOIntDisable(REF_PORT_BASE, REF_PIN);
-    GPIOIntClear(REF_PORT_BASE, REF_PIN);
-}
-
-
-
-void quadEncoderCalibrate(void)
-{
-    isCalibrated = false;
-    GPIOIntEnable(REF_PORT_BASE, REF_PIN);
-}
-
-
-
-bool quadEncoderIsCalibrated(void)
-{
-    return isCalibrated;
-}*/
-
-
-
+// Resets the running encoder count to zero
 void quadEncoderResetCount(void)
 {
     encoderCount = 0;
 }
 
 
+// Set the running encoder count to the parameter newCount
 void quadEncoderSetCount(uint32_t newCount)
 {
     encoderCount = newCount;
 }
 
 
-
+// Returns the current encoder count
 int32_t quadEncoderGetCount(void)
 {
     return encoderCount;
 }
 
 
-
+// Configure GPIO pins and initialize interrupts
+// Get initial state of channel pins for determining direction on first interrupt
 void quadEncoderInit(void)
 {
-    //Enable GPIO peripheral on used ports
+    // Enable GPIO peripheral on used ports
     SysCtlPeripheralEnable(GPIO_CHANNEL_PERIPH);
-    //SysCtlPeripheralEnable(GPIO_REF_PERIPH);
 
-    //configure channel input pins
+    // Configure channel input pins (active HIGH)
     GPIOPadConfigSet(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPD);
     GPIODirModeSet(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN, GPIO_DIR_MODE_IN);
 
-    /*//configure reference input pin
-    GPIOPadConfigSet(REF_PORT_BASE, REF_PIN, GPIO_STRENGTH_4MA, GPIO_PIN_TYPE_STD_WPU);
-    GPIODirModeSet(REF_PORT_BASE, REF_PIN, GPIO_DIR_MODE_IN);*/
-
-    //Initialize channel interrupts
+    // Initialize channel interrupts
     GPIOIntRegister(CHANNEL_PORT_BASE, quadEncoderIntHandler);
     GPIOIntTypeSet(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN, GPIO_BOTH_EDGES);
-    //Get initial state of channel A and channel B for initially determining direction
-    initialPinState = GPIOPinRead(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN);
-    //Enable encoder interrupts
-    GPIOIntEnable(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN);
 
-    /*//initialize reference interrupt but do not enable
-    GPIOIntRegister(REF_PORT_BASE, quadEncoderRefIntHandler);
-    GPIOIntTypeSet(REF_PORT_BASE, REF_PIN, GPIO_FALLING_EDGE);*/
+    // Get initial state of channel A and channel B for initially determining direction
+    initialPinState = GPIOPinRead(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN);
+
+    // Enable encoder interrupts
+    GPIOIntEnable(CHANNEL_PORT_BASE, CHANNEL_A_PIN | CHANNEL_B_PIN);
 }
--- yaw.h
+++ yaw.h
@@ -1,44 +1,41 @@
-#ifndef YAW_H_
-#define YAW_H_
-
 //************************************************************************
-// yaw.h
-//
+// yaw.c
 // Helicopter project
-//
-// Group:       A03 Group 10
-// Created:     16/4/18
-// Last Edited: 16/4/18
+// Group: A03 Group 10
+// Last Edited: 31-5-18
 //
 // Purpose: Handles the yaw of the helicopter
 //************************************************************************
 
+#ifndef YAW_H_
+#define YAW_H_
+
 #include <stdint.h>
 #include <stdbool.h>
 
-void yawRefIntHandler(void);
+
+// Initiate Calibration
+// Enables yaw calibration interrupt
 void yawCalibrate(void);
+
+
+// Return true if yaw has been calibrated against the reference position
 bool yawIsCalibrated(void);
 
-//************************************************************************
-//
-//************************************************************************
+
+// Initialize quadrature encoder and configure yaw reference GPIO pin and interrupt
 void yawInit(void);
 
 
-//************************************************************************
+// Return the current yaw in degrees
 //
-//************************************************************************
+// Parameters:
+//   int32_t precision    scale factor for retaining accuracy with integers
 int32_t yawGetDegrees(int32_t precision);
 
 
-//************************************************************************
-//
-//************************************************************************
-void yawCalibrate(void); //perhaps not part of yaw module???
-
-
+// Remove excess factors of 360 degrees from yaw
+// Returns true if the adjusted yaw value is less than 180 degree
 bool yawClipTo360Degrees(void);
-
 
 #endif /*YAW_H_*/
